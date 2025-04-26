38085e5af62599591aa33e2eb8c9d4f9d4b6a0fa
Gaurav Meena <meenajigaurav9589@gmail.com>
2025-04-26 19:46:42 +0530

    form working

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-let tablinks=document.getElementsByClassName("tab-links");
-let tabContents=document.getElementsByClassName("tab-contents");
 
-
-function opentab(tabname){
-    for (tablink of tablinks ){
-        tablink.classList.remove('active-link');
-        
-    }
-    for (tabcont of tabContents ){
-        tabcont.classList.remove('active-tab');
-
-    }
-    event.currentTarget.classList.add('active-link');
-    document.getElementById(tabname).classList.add('active-tab')
-
+function openMenu() {
+    document.getElementById("sidemenu").style.right = "0";
 }
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxuiE6Lblm5DXVLilaiiv6TrqNyLh12cY7-v5tKz8kHbwpVvQXqmYzBl_RJWjLgkUxJ/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById('msg')
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = 'Message sent successfully! 🎉'
-            setTimeout(function(){
-                msg.innerHTML = ''
-            }, 2000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
 
 
-var sidemenu=document.getElementById('sideMenu');
-function openMenu(){
-    sidemenu.style.right='0';
-
+function closeMenu() {
+    document.getElementById("sidemenu").style.right = "-200px";
 }
-function closeMenu(){
-    sidemenu.style.right='-200px'
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (var tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (var tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var contactForm = document.getElementById("contact-form");
+
+if (contactForm) {
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+        alert("Thank you for contacting me!");
+        contactForm.reset(); 
+    });
 }
